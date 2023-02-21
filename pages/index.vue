aab005fa8be69987ddb5202704d7d5d962522a7e
SaleenaPrajapati <saleenaprajapati1@gmail.com>
2023-02-21 11:35:43 +0545

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <Header />
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+export default {
+  components: { Header },
+};
+</script>
+<style>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+</style>
